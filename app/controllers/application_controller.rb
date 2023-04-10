d722a3605a54e03443b61e85bf5da214dd8bb124
Santiago Acosta <sacostal@chicagobooth.edu>
2023-04-10 01:42:39 +0000

    Paper is functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end
+
+
   def play_rock
     #write a ton of code
     
@@ -13,6 +18,19 @@ class ApplicationController < ActionController::Base
     # render({ :html => "<h1>Hello, world!<h1>".html_safe})
     render({ :template => "game_templates/user_rock.html.erb"}) # Create "game_templates" folder in app/views. Create "user_rock.html.erb" file within the new "game_templates" folder
 
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+     if @comp_move == "rock"
+      @outcome = "won"
+     elsif @comp_move == "paper"
+      @outcome = "tied"
+     elsif @comp_move == "scissors" 
+      @outcome = "lost"
+     end 
+    render({ :template => "game_templates/user_paper.html.erb"})
   end
 
 end
